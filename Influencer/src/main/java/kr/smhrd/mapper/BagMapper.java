9d471e56e021a18341ef66af702e504e393accd5
GITCT <GITCT@DESKTOP-MCB8PI3>
2022-08-16 15:28:02 +0900

    통합본

--- Influencer/src/main/java/kr/smhrd/mapper/BagMapper.java
+++ Influencer/src/main/java/kr/smhrd/mapper/BagMapper.java
@@ -9,6 +9,7 @@ import kr.smhrd.model.BagVO;
 import kr.smhrd.model.BoardVO;
 import kr.smhrd.model.Criteria;
 import kr.smhrd.model.New_BagVO;
+import kr.smhrd.model.Review;
 import kr.smhrd.model.Used_BagVO;
 
 public interface BagMapper {
@@ -19,13 +20,29 @@ public interface BagMapper {
 
 	public Map upload(MultipartHttpServletRequest multipart, String path);
 
-	public List<New_BagVO> new_bag_detail(int bag_no);
-
-	public List<Used_BagVO> used_bag_detail();
-
 	public New_BagVO selectimage(int bag_no);
+	
+	// 에이젝스로 바꿈
+	public List<New_BagVO> new_bag_detail(int bag_no);
+				// 에이젝스로 바꿈
+	public List<Used_BagVO> used_bag_detail(int bag_no);
 
-	public int search_answer(String bag_name_new);
+	// 리뷰
+	public List<Review> review(int bag_no);
+	//리뷰이미지
+	public Review selectreviewimage(int bag_no);
+	// 상세 메인 이미지
+	public BagVO selectmainimage(int bag_no);
+	// 상세 몰 이미지
+	public New_BagVO selectmallimage(int new_bag_no);
+	// 상세 메인 이름
+	public List<BagVO> bagname(int bag_no);
+	// 상세 중고 이미지
+	public Used_BagVO selectusedimage(int used_bag_no);
+	// 상세 중고 몰 이미지
+	public Used_BagVO selectusdemallimage(int used_bag_no);
+	// 리뷰 이미지
+	public BagVO selectallimage(int bag_no);
 
 
 }
--- Influencer/src/main/java/kr/smhrd/mapper/BoardMapper.java
+++ Influencer/src/main/java/kr/smhrd/mapper/BoardMapper.java
@@ -34,3 +34,7 @@ public interface BoardMapper {
 	
 	
 }
+
+
+
+
