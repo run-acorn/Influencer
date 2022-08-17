5c377af34b8fdc87a631b4af900cc4cb13154a05
run-acorn <89288838+run-acorn@users.noreply.github.com>
2022-08-17 18:26:53 +0900

    이상희-상품상세보기 페이지 css 변경

--- Influencer/src/main/java/kr/smhrd/model/Used_BagVO.java
+++ Influencer/src/main/java/kr/smhrd/model/Used_BagVO.java
@@ -7,7 +7,7 @@ public class Used_BagVO {
 
 	private int used_bag_no;
 	   private int bag_no;
-	   private byte[] used_img;
+	   private String used_img;
 	   private int used_price;
 	   private byte[] used_mall;
 	   private String used_link;
